9d074606726411035936c477078be46373c9fc6b
Josh King <joshkking@gmail.com>
2023-05-31 23:20:13 -0400

    small fix on the fp in previous commit

--- EduSocialMediaDSHelper/load_legos.py
+++ EduSocialMediaDSHelper/load_legos.py
@@ -1,9 +1,19 @@
 # %%
 import pandas as pd
+import pkg_resources
 # %%
 def load_data():
-    df_sets = pd.read_csv('legos/sets.csv')
-    df_color_counts = pd.read_csv('legos/color_counts.csv')
+
+    resource_package = __name__  
+
+    sets_resource_path = '/'.join(('legos', 'sets.csv'))
+    sets_file_path = pkg_resources.resource_filename(resource_package, sets_resource_path)
+
+    ccounts_resource_path = '/'.join(('legos', 'color_counts.csv'))
+    ccounts_file_path = pkg_resources.resource_filename(resource_package, ccounts_resource_path)
+
+    df_sets = pd.read_csv(sets_file_path)
+    df_color_counts = pd.read_csv(ccounts_file_path)
     d_color_palette = dict(zip(
         df_color_counts.color_name
         , '#' + df_color_counts.rgb
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="EduSocialMediaDSHelper", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Josh King",
     author_email="joshkking@gmail.com",
     description="Helper for DS Social Media learning material",
